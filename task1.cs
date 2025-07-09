8ff6ed4179affa61fe32a0afe3d28d03d6dfe1af
Ziad  Ibrahim Mohammed Mahmoud <85478356+ziaddevv@users.noreply.github.com>
2025-07-09 14:40:56 +0300

    Update task1.cs

--- task1.cs
+++ task1.cs
@@ -1,1 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("enter a number ");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        if (number > 0)
+            Console.WriteLine("the number is positive");
+        else if (number < 0)
+            Console.WriteLine("the number is negative");
+        else
+            Console.WriteLine("the number is zero");
+    }
+}
 
